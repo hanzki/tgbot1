b09c24a6670142884bec07bdbe409200d5ed5012
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2016-07-21 21:04:40 +0300

    bot

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var request = require('request')
+
+console.log('Loading function');
+
+exports.handler = (event, context, callback) => {
+
+  //Lets configure and request
+  request({
+      url: 'https://api.telegram.org/' + event.token +  '/sendmessage', //URL to hit
+      method: 'POST',
+      //Lets post the following key/values as form
+      json: {
+          chat_id: event.update.message.chat.id,
+          text: 'bot: ' + event.update.message.text
+      }
+  }, function(error, response, body){
+      if(error) {
+          console.log(error);
+          callback(error);
+      } else {
+          console.log(response.statusCode, body);
+          callback(null, "Ok");
+  }
+  });
+};
