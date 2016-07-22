6e8dfe73b0d49cb5b941b1ab2f6e2cf263b73e75
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2016-07-22 23:20:02 +0300

    fixed hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PYTHON_INTERPRETER_PATH='which python'
+PYTHON_INTERPRETER_PATH="$(which python)"
 
 echo -e "[localhost]\n127.0.0.1 ansible_python_interpreter=$PYTHON_INTERPRETER_PATH" > hosts
