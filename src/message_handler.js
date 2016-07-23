d46a0401239dc1e6646b33137a6aa6ab32d928cf
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2016-07-24 01:24:56 +0300

    hot fix

--- src/message_handler.js
+++ src/message_handler.js
@@ -2,39 +2,43 @@
 
 var request = require('request');
 
-exports.handleMessage = (message, config, callback) => {
+var self = {
+  handleMessage: (message, config, callback) => {
 
-  console.log("Message received:", message);
+    console.log("Message received:", message);
 
-  var response = "";
+    var response = "";
 
-  if(message.text === "Meow") {
-    response = "Meow!";
-  } else {
-    response = reverseString(message.text);
+    if(message.text === "Meow") {
+      response = "Meow!";
+    } else {
+      response = self.reverseString(message.text);
+    }
+
+    //Lets configure and request
+    request({
+        url: 'https://api.telegram.org/' + config.telegramBotToken + '/sendmessage', //URL to hit
+        method: 'POST',
+        //Lets post the following key/values as form
+        json: {
+            chat_id: message.chat.id,
+            text: response
+        }
+    }, function(error, response, body){
+        if(error) {
+            console.log(error);
+            callback(error);
+        } else {
+            console.log(response.statusCode, body);
+            callback(null, "Ok");
+        }
+    });
+
+  },
+
+  reverseString: (string) => {
+    return string.split("").reverse().join("");
   }
-
-  //Lets configure and request
-  request({
-      url: 'https://api.telegram.org/' + config.telegramBotToken + '/sendmessage', //URL to hit
-      method: 'POST',
-      //Lets post the following key/values as form
-      json: {
-          chat_id: message.chat.id,
-          text: response
-      }
-  }, function(error, response, body){
-      if(error) {
-          console.log(error);
-          callback(error);
-      } else {
-          console.log(response.statusCode, body);
-          callback(null, "Ok");
-      }
-  });
-
 };
 
-exports.reverseString = (string) => {
-  return string.split("").reverse().join("");
-};
+module.exports = self;
