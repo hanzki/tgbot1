2b50ef3e165f4eea67b50b9a6592e5f6f37e942f
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2016-07-24 12:41:16 +0300

    changed cf template

--- src/message_handler.js
+++ src/message_handler.js
@@ -2,43 +2,44 @@
 
 var request = require('request');
 
-var self = {
-  handleMessage: (message, config, callback) => {
+var _handleMessage = (message, config, callback) => {
 
-    console.log("Message received:", message);
+  console.log("Message received:", message);
 
-    var response = "";
+  var response = "";
 
-    if(message.text === "Meow") {
-      response = "Meow!";
-    } else {
-      response = self.reverseString(message.text);
-    }
-
-    //Lets configure and request
-    request({
-        url: 'https://api.telegram.org/' + config.telegramBotToken + '/sendmessage', //URL to hit
-        method: 'POST',
-        //Lets post the following key/values as form
-        json: {
-            chat_id: message.chat.id,
-            text: response
-        }
-    }, function(error, response, body){
-        if(error) {
-            console.log(error);
-            callback(error);
-        } else {
-            console.log(response.statusCode, body);
-            callback(null, "Ok");
-        }
-    });
-
-  },
-
-  reverseString: (string) => {
-    return string.split("").reverse().join("");
+  if(message.text === "Meow") {
+    response = "Meow!";
+  } else {
+    response = self.reverseString(message.text);
   }
+
+  //Lets configure and request
+  request({
+      url: 'https://api.telegram.org/' + config.telegramBotToken + '/sendmessage', //URL to hit
+      method: 'POST',
+      //Lets post the following key/values as form
+      json: {
+          chat_id: message.chat.id,
+          text: response
+      }
+  }, function(error, response, body){
+      if(error) {
+          console.log(error);
+          callback(error);
+      } else {
+          console.log(response.statusCode, body);
+          callback(null, "Ok");
+      }
+  });
+
 };
 
-module.exports = self;
+var _reverseString = (string) => {
+  return string.split("").reverse().join("");
+};
+
+module.exports = {
+  handleMessage: _handleMessage,
+  reverseString: _reverseString
+}
