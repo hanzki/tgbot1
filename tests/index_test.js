2a4b32a12c986cb09ca57755d0ae740731ea919e
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2016-08-07 14:10:34 +0300

    forgot a comma from the cloudformation.json

--- tests/index_test.js
+++ tests/index_test.js
@@ -3,23 +3,30 @@ var sinon = require('sinon');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var index = require('./../src/index');
 var messageHandler = require('./../src/message_handler');
+var commandHandler = require('./../src/command_handler');
+var util = require('./../src/util');
 
 describe('Index', function() {
   beforeEach(function() {
-    sinon.stub(messageHandler, 'handleMessage', function(message, config, callback) {
+    sinon.stub(messageHandler, 'handleMessage').callsArgWith(2, null, "Ok");
+    sinon.stub(commandHandler, 'handleCommand', (message, command, config, callback) => {
       setTimeout(function() {
         callback(null, "Ok");
       }, 0);
     });
+    sinon.stub(util, 'parseCommand').callsArg(1);
+    util.parseCommand.withArgs({ text: "/foo bar" }).callsArgWith(1, null, {command: "/foo", args: ["bar"]});
   });
 
   afterEach(function() {
     messageHandler.handleMessage.restore();
+    commandHandler.handleCommand.restore();
+    util.parseCommand.restore();
   });
 
   it('handleMessage() should be called in case of message update', function(done) {
     var message = { text: "Hello" };
-    var config = { key: "value" };
+    var config = {};
 
     index.handler(
       {
@@ -29,8 +36,29 @@ describe('Index', function() {
       null,
       function (error, response) {
         expect(response).to.equal("Ok");
+        expect(messageHandler.handleMessage.calledOnce).to.be.true;
         expect(messageHandler.handleMessage.getCall(0).args[0]).to.equal(message);
         expect(messageHandler.handleMessage.getCall(0).args[1]).to.equal(config);
+        done();
+      }
+    );
+  });
+
+  it('handleCommand() should be called in case of command', function(done) {
+    var message = { text: "/foo bar" };
+    var config = {};
+
+    index.handler(
+      {
+        config: config,
+        update: { message: message }
+      },
+      null,
+      function (error, response) {
+        expect(response).to.equal("Ok");
+        expect(commandHandler.handleCommand.calledOnce).to.be.true;
+        expect(commandHandler.handleCommand.getCall(0).args[0]).to.equal(message);
+        expect(commandHandler.handleCommand.getCall(0).args[2]).to.equal(config);
         done();
       }
     );
