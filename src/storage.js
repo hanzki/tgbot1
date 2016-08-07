798042f60afc3a29619c6687c02cdf486a8723a1
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2016-08-07 14:16:37 +0300

    fixed dynamodb storing

--- src/storage.js
+++ src/storage.js
@@ -19,7 +19,7 @@ module.exports.getValue = (table, id, callback) => {
 module.exports.storeValue = (table, key, value, callback) => {
 
   var params = {
-    Key: {
+    Item: {
       Id: {
         S: key
       },
